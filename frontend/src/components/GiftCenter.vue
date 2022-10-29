7f002d1faf71c28fdf22cb0d4cbc93cfd75ffca2
zhangjc20 <zhangjc20@163.com>
2022-10-29 23:09:16 +0800

    包装按钮组件

--- frontend/src/components/GiftCenter.vue
+++ frontend/src/components/GiftCenter.vue
@@ -14,26 +14,83 @@
         </svg> = 240
       </div>
       <div class="exchange-area">
-        兑换
-        <el-input class="value-input" v-model="input" placeholder="请输入金额" clearable />
+        兑换 ： 
+        <el-input class="value-input" v-model="valueInput" placeholder="请输入金额" clearable />
+        &nbsp;&nbsp;元
+      </div>
+      <div class="confirm-exchange-area">
+        <el-button class="confirm-exchange-btn" @click="handleClickExchange">确认兑换</el-button>
       </div>
     </div>
 </template>
   
   <script>
+  import { ElMessage, ElMessageBox } from 'element-plus';
   export default {
     name: 'GiftCenter',
     props: {
     },
     data(){
         return {
+          valueInput:""
         }
+    },
+    methods:{
+      getEqualDonuts(rate){
+          return Number(this.valueInput)*rate;
+      },
+      handleClickExchange(){
+        ElMessageBox.confirm(
+          '您将要花费'+this.getEqualDonuts(120)+'甜甜圈兑换'+this.valueInput+'元',
+          '确认兑换',
+          {
+            confirmButtonText: 'OK',
+            cancelButtonText: 'Cancel',
+            type: 'warning',
+            draggable: true,
+          }
+        )
+          .then(() => {
+            ElMessage({
+              type: 'success',
+              message: '兑换成功',
+            })
+          })
+          .catch(() => {
+            ElMessage({
+              type: 'info',
+              message: '取消兑换',
+            })
+          })
+      }
     }
   }
   </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped lang="scss">
+  .confirm-exchange-btn {
+    background-color: #FBE484;
+    border-color: #FBE484;
+    color: #606266;
+  }
+  .confirm-exchange-btn:focus {
+    background-color: #e8d175;
+    border-color: #e8d175;
+    color: #606266;
+  }
+  .confirm-exchange-btn:hover {
+    background-color: #e2cc73;
+    border-color: #e2cc73;
+    color: #606266;
+  }
+  .confirm-exchange-area {
+    margin-top: 20px;
+  }
+  .exchange-area {
+    margin-top: 40px;
+    font-family: YiPinChuangXiang;
+  }
   .icon {
   width: 1em;
   height: 1em;
@@ -42,7 +99,7 @@
   overflow: hidden;
 }
 .value-input {
-  width: 300px;
+  width: 200px;
 }
   .gift-title {
     margin-top: 20px;
