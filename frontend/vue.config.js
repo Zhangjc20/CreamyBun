5ec65b9ea7912660cbc6e588c029e2f0e84552c9
zhangjc20 <zhangjc20@163.com>
2022-11-05 15:35:13 +0800

    首页暂时完成

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true,           // 是否自动打开项目
+    host: '127.0.0.1',      // 制定域名
+    port: 8081,           // 端口号
+  }
 })
