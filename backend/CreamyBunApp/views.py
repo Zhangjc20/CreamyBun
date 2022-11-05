b564732175ac0f055d6346d0585a917c758eef27
zhangjc20 <zhangjc20@163.com>
2022-11-05 23:53:35 +0800

    补充上次未提交内容

--- backend/CreamyBunApp/views.py
+++ backend/CreamyBunApp/views.py
@@ -7,8 +7,25 @@ import json
 
 # 注册
 def log_up(request):
-
-    return HttpResponse(json.dumps({'niha':'nihaosahi'}), content_type='application/json')
+    query_dict = request.GET
+    type = query_dict.get("type","")
+    #如果是发邮箱,verifyCode返回应该的验证码
+    if type == 'getEmail':
+        pass
+        #return HttpResponse(json.dumps({'status':'ok','verifyCode':}), content_type='application/json')
+        return HttpResponse(json.dumps({'status':'ok'}), content_type='application/json')
+    email = query_dict.get("email","")
+    username = query_dict.get("username","")
+    #判断是否重名
+    flag = exist_user(username)
+    if flag:
+        return HttpResponse(json.dumps({'status':'wrong','type':'sameName'}), content_type='application/json')
+    password = query_dict.get("password","")
+    flag = add_a_user(username=username,password=password,email=email)
+    if flag:
+        return HttpResponse(json.dumps({'status':'ok'}), content_type='application/json')
+    else:
+        return HttpResponse(json.dumps({'status':'wrong','type':'unknown'}), content_type='application/json')
 
 # 注销
 def log_off(request):
