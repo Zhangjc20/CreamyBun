13f0d174e53d80dbdf31708d349e8e72812ed00c
unknown <zhangfeihong20@126.com>
2022-11-05 17:01:09 +0800

    后端基础类基本搭完

--- backend/CreamyBunApp/classDefination/taskClass.py
+++ backend/CreamyBunApp/classDefination/taskClass.py
@@ -1,26 +1,25 @@
-class Task:
-    def __init__(self,ID,poster,task_name,description,problem_total_number,problem_number_for_single_receiver,star_rank,single_bonus,release_mode,begin_time,end_time):
-        self.ID=ID
-        self.poster=poster #发布者的id
-        self.task_name=task_name
-        self.description=description
+from .problemClass import *
+from ..variables.globalConstants import MAX_TASK_NAME_LENGTH,MAX_DESCRIPTION_LENGTH,\
+                                        MAX_TIME_STRING_LENGTH
 
-        # 通过以下两个变量计算该任务的完成程度
-        self.problem_total_number=problem_total_number
-        self.finished_problem_number=0
+class Task(models.Model):
+    poster = models.BigIntegerField(default=-1) # 发布者的id
+    task_name = models.CharField(max_length = MAX_TASK_NAME_LENGTH,default="task_name") # 任务名
+    description = models.CharField(max_length = MAX_DESCRIPTION_LENGTH,default="task_description") # 任务描述
 
-        # 每个用户每次可领取的题目数量
-        self.problem_number_for_single_receiver = problem_number_for_single_receiver
+    # 通过以下两个变量计算该任务的完成程度
+    problem_total_number = models.IntegerField(default=-1) # 任务中大题总数
+    finished_problem_number = models.IntegerField(default=0) # 任务中已经完成的大题数
 
-        self.star_rank=star_rank
-        self.single_bonus=single_bonus
-        self.release_mode=release_mode #见相关常量
+    # 每个用户每次可领取的题目数量
+    problem_number_for_single_receiver = models.IntegerField(default=-1)
 
-        # 该时间须使用datetime.datetime.now()得到，可通过成员变量获取年月日时分秒等属性，通过strftime转成字符串
-        # 或者通过前端获取的年月日时分秒等属性转成datetime相关类型
-        # 参考https://www.cnblogs.com/jszfy/p/11143048.html
-        self.begin_time=begin_time
-        self.end_time=end_time
-         
-        self.receiver_list=[] # 该任务的所有领取者，成员为用户ID
-        self.problem_list=[] # 该任务的所有大题，成员为Problem类实例
+    star_rank = models.IntegerField(default=-1) # 任务星级
+    single_bonus = models.IntegerField(default=-1) # 任务单题奖励
+    release_mode = models.IntegerField(default=0) # 任务发布模式
+
+    begin_time = models.CharField(max_length=MAX_TIME_STRING_LENGTH,default="begin_time") # 任务开始时间
+    end_time = models.CharField(max_length=MAX_TIME_STRING_LENGTH,default="end_time") # 任务结束时间
+
+    receiver_list = models.ManyToManyField(Int) # 该任务的所有领取者，成员为用户ID
+    problem_list = models.ManyToManyField(Problem) # 该任务的所有大题，成员为Problem类实例
--- backend/CreamyBunApp/classDefination/universalClass.py
+++ backend/CreamyBunApp/classDefination/universalClass.py
@@ -6,4 +6,7 @@ class KeyToValue(models.Model):
     value = models.IntegerField(default=0)
 
 class Str(models.Model):
-    str_content = models.CharField(max_length=MAX_ANSWER_LENGTH)
+    str_content = models.CharField(max_length=MAX_ANSWER_LENGTH,default="")
+
+class Int(models.Model):
+    int_content = models.IntegerField(default=-1)
