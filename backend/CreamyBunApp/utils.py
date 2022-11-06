8a84129e90632f3c208261ffb1c24866b51766cf
ZDandsomSP <1115854107@qq.com>
2022-11-07 01:01:46 +0800

    初步完成文件上传解压并列表，由于zip文件的问题该功能尚不稳定（附带测试用zip上传）

---
+++ backend/CreamyBunApp/utils.py
@@ -0,0 +1,11 @@
+import zipfile
+
+
+def unzip_file(zip_src, dst_dir):
+    r = zipfile.is_zipfile(zip_src)
+    if r:
+        fz = zipfile.ZipFile(zip_src, 'r')
+        for file in fz.namelist():
+            fz.extract(file, dst_dir)
+    else:
+        print('This is not zip')
