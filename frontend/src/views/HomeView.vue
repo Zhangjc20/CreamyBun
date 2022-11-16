135570a3cf3b0d2c7b743f329b6f80df00ffe73b
zhangjc20 <zhangjc20@163.com>
2022-11-16 16:33:00 +0800

    图片框图组件添加选色

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,6 +6,7 @@
     <el-main class="main-style">
       <ImageFramer :dash="dash" :color="color" imageSrc="https://img1.baidu.com/it/u=3171858076,1808175228&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500"/>
       <el-button @click.stop="changeDash">切换虚线</el-button>
+      <el-color-picker v-model="color" />
       <div class="custom-box">
         <div class="custom-title-outer"><span class="custom-title">个性化数据标注服务</span></div>
         <el-row>
