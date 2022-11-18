c46dae10006fe52597e2846e40d926932da7491f
ZDandsomSP <1115854107@qq.com>
2022-11-18 13:01:19 +0800

    拉fhgg

--- frontend/src/components/ReleaseData.vue
+++ frontend/src/components/ReleaseData.vue
@@ -351,7 +351,7 @@ export default {
         description: "",
         questionType: "",
         receiverNum:0,
-        promblemTotalNum: "",
+        problemTotalNum: "",
         releaseMode: "",
         releaseModeInt: "",
         singleBonus: "",
@@ -553,6 +553,7 @@ export default {
     async finalSubmit(){
       this.form.poster = this.username
       this.form.materialType = this.materialType
+      this.form.problemTotalNum = this.$refs.MyMaterialUpload.fullList[0].length
       if(this.questionTypeMixed){
         this.form.questionType = 4
       }
@@ -607,7 +608,6 @@ export default {
           type: 'error'
         });
       });     
-      console.log("xpxpxpxpxpxppxppxpxppxpxppxppxpxppxppxp")
       await axios.post("http://localhost:8000/release_task/",
         {basicInfoForm:this.form,
           questionList:this.questionList,
@@ -681,99 +681,5 @@ export default {
   background: #ddd;
   border-radius: 20px;
 }
-/* 
-  .user-area {
-    float: right;
-    position: absolute;
-    top:0;
-    right: 0;
-    height: 60px;
-    margin-right: 90px;
-  }
-   .avatar {
-    float: right;
-    position: absolute;
-    top:0;
-    right: 0;
-    margin-right: 180px;
-    margin-top: 10px;
-  }
-  .user-center {
-    line-height: 60px;
-    margin-bottom: 20px;
-    font-size: 22px;
-    color:#f8f8f8;
-    font-family: YouSheBlack;
-    cursor: pointer;
-  }
-  .user-center:hover {
-    opacity: 0.6;
-  }
-.nav-title {
-    float: left;
-    height: 60px;
-    font-family: YouSheRound;
-    line-height: 50px;
-    font-size: 28px;
-    color: #5EABBF;
-    margin-left: 80px;
-    display: flex;
-  }
-  .nav-title-font {
-    margin-left: 10px;
-    line-height: 60px;
-  }
-  .menu-out {
-    margin-left: 80px;
-    background-color: #ffffff;
-    width:470px;
-    overflow: hidden;
-    border-radius: 40px;
-    margin-top: 10px;
-    display: flex;
-   }
-   .el-button--primary {
-  background: #FBE484 !important;
-  border-color: #FBE484 !important;
-  color:#6C6C6C;
-}
-.el-button--medium {
-  width: 80px;
-}
 
-.logo {
-  width: 45px; 
-  height: 40px; 
-  margin-top: 10px;
-}
-.login-button {
-  margin-right: 30px;
-}
-
-.log-buttons {
-  float: right;
-  position: absolute;
-  top:0;
-  right: 0;
-  line-height: 60px;
-  margin-right: 100px;
-}
-.el-nav-menu {
-  width:480px;
-  padding-left: 46px;
-}
-.el-menu {
-  border-radius: 0;
-}
-.el-menu-item {
-  height: 50px;
-  font-size: 16px;
-  padding-top: 4px;
-}
-.el-menu--horizontal .el-menu-item:not(.is-disabled):focus{
-  background-color: transparent;
-}
-.el-menu--horizontal .el-menu-item:not(.is-disabled):hover{
-  background-color: #f8f8f8;
-} */
 </style>
