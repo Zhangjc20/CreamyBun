032bf1f05d6ba5c6530e7312104bd35303f441f7
LiuZhiheng <liuzhihe20@mails.tsinghua.edu.cn>
2023-01-08 01:31:28 +0800

    修复bug

--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -153,6 +153,13 @@ export default {
         })
         return;
       }
+      if(!this.imageUrl){
+        ElMessage({
+          type:"warning",
+          message:"请上传反馈图片"
+        })
+        return;
+      }
       if(!this.email.match(/[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+/g)){
         ElMessage({
           type:"warning",
